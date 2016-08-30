481a23f5fbdca0d755491c5340c57cb5dab9e79e
DjAmolGroup <djamolpatil@gmail.com>
2016-08-30 10:48:27 +0530

    Update mail.sh

--- mail.sh
+++ mail.sh
@@ -20,39 +20,39 @@ yum -y install postfix
 #andreagrandi.it change to your domain in configuration setting
 #You can see my complete configuration here:
 
-myhostname = andreagrandi.it
-smtpd_banner = $myhostname ESMTP $mail_name (Ubuntu)
-biff = no
+postconf -e myhostname = andreagrandi.it
+postconf -e smtpd_banner = $myhostname ESMTP $mail_name (Ubuntu)
+postconf -e biff = no
 
 # appending .domain is the MUA's job.
-append_dot_mydomain = no
+postconf -e append_dot_mydomain = no
 
 # Uncomment the next line to generate "delayed mail" warnings
 #delay_warning_time = 4h
 
-readme_directory = no
+postconf -e readme_directory = no
 
 # TLS parameters
-smtpd_tls_cert_file=/etc/ssl/certs/ssl-cert-snakeoil.pem
-smtpd_tls_key_file=/etc/ssl/private/ssl-cert-snakeoil.key
-smtpd_use_tls=yes
-smtpd_tls_session_cache_database = btree:${data_directory}/smtpd_scache
-smtp_tls_session_cache_database = btree:${data_directory}/smtp_scache
+postconf -e smtpd_tls_cert_file=/etc/ssl/certs/ssl-cert-snakeoil.pem
+postconf -e  smtpd_tls_key_file=/etc/ssl/private/ssl-cert-snakeoil.key
+postconf -e smtpd_use_tls=yes
+postconf -e smtpd_tls_session_cache_database = btree:${data_directory}/smtpd_scache
+postconf -e smtp_tls_session_cache_database = btree:${data_directory}/smtp_scache
 
 # See /usr/share/doc/postfix/TLS_README.gz in the postfix-doc package for
 # information on enabling SSL in the smtp client.
 
-smtpd_relay_restrictions = permit_mynetworks permit_sasl_authenticated defer_unauth_destination
-alias_maps = hash:/etc/aliases
-alias_database = hash:/etc/aliases
-virtual_alias_domains = andreagrandi.it
-virtual_alias_maps = hash:/etc/postfix/virtual
-myorigin = /etc/mailname
-mydestination = andreagrandi, localhost.localdomain, localhost
-mynetworks = 127.0.0.0/8 [::ffff:127.0.0.0]/104 [::1]/128
-mailbox_size_limit = 0
-recipient_delimiter = +
-inet_interfaces = all
+postconf -e smtpd_relay_restrictions = permit_mynetworks permit_sasl_authenticated defer_unauth_destination
+postconf -e alias_maps = hash:/etc/aliases
+postconf -e alias_database = hash:/etc/aliases
+postconf -e virtual_alias_domains = andreagrandi.it
+postconf -e virtual_alias_maps = hash:/etc/postfix/virtual
+postconf -e myorigin = /etc/mailname
+postconf -e mydestination = andreagrandi, localhost.localdomain, localhost
+postconf -e mynetworks = 127.0.0.0/8 [::ffff:127.0.0.0]/104 [::1]/128
+postconf -e mailbox_size_limit = 0
+postconf -e recipient_delimiter = +
+postconf -e inet_interfaces = all
 
 
 
