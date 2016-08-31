f293de01173096023a7a3c86aa678e05489a1205
DjAmolGroup <djamolpatil@gmail.com>
2016-08-31 15:43:55 +0530

    Update mail.sh

--- mail.sh
+++ mail.sh
@@ -10,6 +10,17 @@
 
 # INSTALL postfix(Mail Service/server)
 yum -y install postfix
+mkdir /etc;mkdir /etc/ssl;mkdir /etc/ssl/certs;;mkdir /etc/ssl/private;
+openssl req -new -x509 -days 3650 -nodes -out /etc/ssl/certs/postfix.pem -keyout /etc/ssl/private/postfix.pem
+echo -n "Enter some text > "
+read text
+echo "Your Domain: $text"
+MAINDOMAIN = $text
+MAINIP = hostname --ip-address
+echo "And Please Create MX Entry in Your Domain DNS: \n
+Example : 
+mail.$MAINDOMAIN. IN A $MAINIP
+mail.$MAINDOMAIN. IN MX 0 mail.$MAINDOMAIN."
 
 #
 #
@@ -20,40 +31,32 @@ yum -y install postfix
 #andreagrandi.it change to your domain in configuration setting
 #You can see my complete configuration here:
 
-#postconf -e myhostname = andreagrandi.it
-myhostname = mail.domain.com
-smtpd_banner = $myhostname ESMTP $mail_name (Ubuntu)
-biff = no
-
-# appending .domain is the MUA's job.
-append_dot_mydomain = no
-
-# Uncomment the next line to generate "delayed mail" warnings
-#delay_warning_time = 4h
-
+echo -e 'myhostname = mail.'.$MAINDOMAIN.'\nsmtpd_banner = $myhostname ESMTP $mail_name (Ubuntu)
+\nbiff = no
+\nappend_dot_mydomain = no
+\n
 readme_directory = no
+\nsmtpd_tls_cert_file=/etc/ssl/certs/postfix.pem
+\nsmtpd_tls_key_file=/etc/ssl/private/postfix.key
+\nsmtpd_use_tls=yes
+\nsmtpd_tls_session_cache_database = btree:${data_directory}/smtpd_scache
+\nsmtp_tls_session_cache_database = btree:${data_directory}/smtp_scache
+\nsmtpd_relay_restrictions = permit_mynetworks permit_sasl_authenticated defer_unauth_destination
+\nalias_maps = hash:/etc/aliases
+\nalias_database = hash:/etc/aliases
+\nvirtual_alias_domains = '.$MAINDOMAIN.'
+\nvirtual_alias_maps = hash:/etc/postfix/virtual
+\nmyorigin = /etc/mailname
+\nmydestination = mail.'.$MAINDOMAIN.', '.$MAINDOMAIN.', localhost.localdomain, localhost
+\nmynetworks = 127.0.0.0/8 [::ffff:127.0.0.0]/104 [::1]/128
+\nmailbox_size_limit = 0
+\nrecipient_delimiter = +
+\ninet_interfaces = all
 
-# TLS parameters
-smtpd_tls_cert_file=/etc/ssl/certs/postfix.pem
- smtpd_tls_key_file=/etc/ssl/private/postfix.key
-smtpd_use_tls=yes
-smtpd_tls_session_cache_database = btree:${data_directory}/smtpd_scache
-smtp_tls_session_cache_database = btree:${data_directory}/smtp_scache
+' >> /etc/postfix/main.cf
 
-# See /usr/share/doc/postfix/TLS_README.gz in the postfix-doc package for
-# information on enabling SSL in the smtp client.
 
-smtpd_relay_restrictions = permit_mynetworks permit_sasl_authenticated defer_unauth_destination
-alias_maps = hash:/etc/aliases
-alias_database = hash:/etc/aliases
-virtual_alias_domains = domain.com
-virtual_alias_maps = hash:/etc/postfix/virtual
-myorigin = /etc/mailname
-mydestination = mail.domain.com, domain.com, localhost.localdomain, localhost
-mynetworks = 127.0.0.0/8 [::ffff:127.0.0.0]/104 [::1]/128
-mailbox_size_limit = 0
-recipient_delimiter = +
-inet_interfaces = all
+
 
 
 
@@ -62,48 +65,45 @@ inet_interfaces = all
 
 #Edit /etc/postfix/virtual file and add your aliases, one per line, like in this example:
 
-
-info@domain.com youremail@gmail.com
-sales@domain.com youremail@live.com
-
+echo -e 'info@'.$MAINDOMAIN.' djamolpatil@gmail.com
+\nsupport@'.$MAINDOMAIN.' djamolpatil@gmail.com' >> /etc/postfix/virtual
 
 
 
 
-
- postmap /etc/postfix/virtual
-/etc/init.d/postfix reload
-
+service postfix start
+postmap /etc/postfix/virtual
+service postfix reload
 
 
 ################  All Emails for to one Email Address ##################
 
 #Create /etc/postfix/virtual-regexp with the following content:
-/.+@.+/ email@gmail.com
+#/.+@.+/ email@gmail.com
 
 #Edit /etc/postfix/main.cf and add regexp:/etc/postfix/virtual-regexp to the virtual_maps configuration. The end result might look like this in main.cf:
 
-virtual_alias_maps = hash:/etc/postfix/virtual, regexp:/etc/postfix/virtual-regexp
+#virtual_alias_maps = hash:/etc/postfix/virtual, regexp:/etc/postfix/virtual-regexp
 
 #Build the mapfile by typing command:
 
-postmap /etc/postfix/virtual-regexp
+#postmap /etc/postfix/virtual-regexp
 
 #This also requires a virtual.db to exist. If it doesn't create an empty file called virtual and run :
 
-touch /etc/postfix/virtual && postmap /etc/postfix/virtual
+#touch /etc/postfix/virtual && postmap /etc/postfix/virtual
 
 
 
 ##############   Generate Ssl cerificate ################
-mkdir /etc/ssl/private/
+#mkdir /etc/ssl/private/
 
 #openssl req -new -x509 -days 3650 -nodes -out /etc/ssl/certs/dovecot.pem -keyout /etc/ssl/private/dovecot.pem
 #chmod o= /etc/ssl/private/dovecot.pem
 
-openssl req -new -x509 -days 3650 -nodes -out /etc/ssl/certs/postfix.pem -keyout /etc/ssl/private/postfix.pem
-postconf -e smtpd_tls_cert_file=/etc/ssl/certs/postfix.pem
-postconf -e smtpd_tls_key_file=/etc/ssl/private/postfix.pem
+#openssl req -new -x509 -days 3650 -nodes -out /etc/ssl/certs/postfix.pem -keyout /etc/ssl/private/postfix.pem
+#postconf -e smtpd_tls_cert_file=/etc/ssl/certs/postfix.pem
+#postconf -e smtpd_tls_key_file=/etc/ssl/private/postfix.pem
 #service dovecot restart
 
-service postfix restart
+#service postfix restart
