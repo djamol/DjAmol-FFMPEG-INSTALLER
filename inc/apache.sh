7f1e3a293c6ae2b32538e15290e967ac08a7047c
DjAmolGroup <djamolpatil@gmail.com>
2016-08-31 22:25:46 +0530

    Update apache.sh

--- inc/apache.sh
+++ inc/apache.sh
@@ -27,8 +27,8 @@ tar -xzf httpd-2*
 fi
 
 
-cp -r apr-1.5.2 httpd-2.4.18/srclib/apr
-cp -r apr-util-1.5.4 httpd-2.4.18/srclib/apr-util
+cp -r  $SCRIPTPATH/src/apr-1.5.2 $SCRIPTPATH/src/httpd-2.4.18/srclib/apr
+cp -r $SCRIPTPATH/src/apr-util-1.5.4 $SCRIPTPATH/src/httpd-2.4.18/srclib/apr-util
 
 cd httpd-2.4.18
 #./configure  --enable-so --with-included-apr --with-mpm=event prefix=/usr/local/apache2 --with-pcre=/usr/local/pcre --with-ssl=/opt/ssl LDFLAGS=-L/opt/ssl/lib CPPFLAGS=-I/opt/ssl/include --disable-v4-mapped --enable-access-compat=static --enable-actions=static --enable-alias=static --enable-asis=static --enable-auth_basic=static --enable-authn_core=static --enable-authn_file=static --enable-authz_core=static --enable-authz_groupfile=static --enable-authz_host=static --enable-authz_user=static --enable-autoindex=static --enable-cgi=static --enable-deflate=static --enable-dir=static --enable-env=static --enable-expires=static --enable-filter=static --enable-headers=static --enable-include=static --enable-log_config=static --enable-logio=static --enable-mime=static --enable-modules=none --enable-negotiation=static --enable-proxy=static --enable-proxy-connect=static --enable-proxy-http=static --enable-rewrite=static --enable-setenvif=static --enable-slotmem_shm=static --enable-socache_dbm=static --enable-socache_shmcb=static --enable-ssl=static --enable-status=static  --enable-unique-id=static --enable-unixd=static --enable-userdir=static --enable-version=static
