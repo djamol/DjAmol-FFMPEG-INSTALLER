6fe3c9bc9653968052f03b716e286bd3b8205b78
DjAmolGroup <djamolpatil@gmail.com>
2016-09-01 11:00:58 +0530

    Update domain.sh

--- inc/domain.sh
+++ inc/domain.sh
@@ -2,5 +2,6 @@ dstring="test,POWer,TODAY, Fres styel,netwo.in"
 IFS=', ' read -r -a array <<< "$dstring"
 for element in "${array[@]}"
 do
-    echo "$element"
+    echo -e '<VirtualHost '$element':80>\nServerName '$element'\nServerAlias www.'$element'\nDocumentRoot /home/www/domain/'$element'\nServerAdmin webmaster@djamol.com\n</VirtualHost>' >> /usr/local/apache2/conf/httpd.conf
+    echo "Domain Added :$element (Directory:/home/www/domain/$element)"
 done
