51488f81bde606692a484b7c94b66cfbb3c5cfed
DjAmolGroup <djamolpatil@gmail.com>
2016-09-01 13:01:03 +0530

    Update ftp.sh

--- inc/ftp.sh
+++ inc/ftp.sh
@@ -4,11 +4,11 @@
 # djamolpatil@gmail.com
 
 ## Pro ftpd installation
-rpm -ivh http://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-6.noarch.rpm
+rpm -ivh http://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-8.noarch.rpm
 yum -y install proftpd proftpd-utils
+cp -f $SCRIPTPATH/conf/proftpd.conf /etc/proftpd.conf
 systemctl start proftpd.service
 systemctl enable proftpd.service
-proftpd -v
 
 ## INSTALLation End proftpd
 date +"%r" >> $BUILD;echo "PROFTPD Program Complete" >> $BUILD
