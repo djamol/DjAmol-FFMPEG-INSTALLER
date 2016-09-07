8e7484732d4c7aa70754cb77b1ffc8f59cb648e4
DjAmolGroup <djamolpatil@gmail.com>
2016-09-07 14:39:21 +0530

    Update database_backup.sh

--- database_backup.sh
+++ database_backup.sh
@@ -1,6 +1,6 @@
 #Structure With Data Backup
-DATABASE_LIST=$(mysql -NBe 'show schemas' | grep -wv 'mysql\|personnel\|buildings\|information_schema\|performance_schema');echo $DATABASE_LIST;mysqldump --databases $DATABASE_LIST > home/www/bk.sql
+DATABASE_LIST=$(mysql -NBe 'show schemas' | grep -wv 'mysql\|personnel\|buildings\|information_schema\|performance_schema');echo $DATABASE_LIST;mysqldump --databases $DATABASE_LIST > bk.sql
 
 
 #Structure only backup
-DATABASE_LIST=$(mysql -NBe 'show schemas' | grep -wv 'mysql\|personnel\|buildings\|information_schema\|performance_schema');echo $DATABASE_LIST;mysqldump --databases $DATABASE_LIST > home/www/structure.sql
+DATABASE_LIST=$(mysql -NBe 'show schemas' | grep -wv 'mysql\|personnel\|buildings\|information_schema\|performance_schema');echo $DATABASE_LIST;mysqldump --databases $DATABASE_LIST > structure.sql
