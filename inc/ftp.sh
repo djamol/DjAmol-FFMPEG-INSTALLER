6670dc19c152f2aee1449cbeb89ade250b0c8aef
DjAmolGroup <djamolpatil@gmail.com>
2016-09-07 11:32:07 +0530

    Update ftp.sh

--- inc/ftp.sh
+++ inc/ftp.sh
@@ -7,6 +7,11 @@
 rpm -ivh http://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-8.noarch.rpm
 yum -y install proftpd proftpd-utils
 cp -f $SCRIPTPATH/conf/proftpd.conf /etc/proftpd.conf
+ mkdir /etc/proftpd; touch /etc/proftpd/passwd.vhosts;chmod 640 /etc/proftpd/passwd.vhosts
+cp -f $SCRIPTPATH/conf/ftpd-rsa.pem /etc/proftpd/ftpd-rsa.pem
+cp -f $SCRIPTPATH/conf/ftpd-rsa-key.pem /etc/proftpd/ftpd-rsa-key.pem
+ chmod 600 /etc/proftpd/*.pem
+
 systemctl start proftpd.service
 systemctl enable proftpd.service
 
