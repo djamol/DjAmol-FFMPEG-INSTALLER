e7213f5d1acdeba9d83382a38b7071b305140ee5
DjAmolGroup <djamolpatil@gmail.com>
2016-10-09 11:50:18 +0530

    Update database_backup.sh

--- database_backup.sh
+++ database_backup.sh
@@ -4,3 +4,13 @@ DATABASE_LIST=$(mysql -NBe 'show schemas' | grep -wv 'mysql\|personnel\|building
 
 #Structure only backup
 DATABASE_LIST=$(mysql -NBe 'show schemas' | grep -wv 'mysql\|personnel\|buildings\|information_schema\|performance_schema');echo $DATABASE_LIST;mysqldump --no-data --databases $DATABASE_LIST > structure.sql
+
+
+
+
+
+# SQL Commands
+//delete all records/row
+DELETE FROM `table`
+//null value delete rows only example
+DELETE FROM `table` WHERE `gmc` ='null';
