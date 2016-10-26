b407eac3af441c5e0824d75cebdcad743a4096b4
DjAmolGroup <djamolpatil@gmail.com>
2016-10-26 22:33:22 +0530

    Update database_backup.sh

--- database_backup.sh
+++ database_backup.sh
@@ -6,7 +6,13 @@ DATABASE_LIST=$(mysql -NBe 'show schemas' | grep -wv 'mysql\|personnel\|building
 DATABASE_LIST=$(mysql -NBe 'show schemas' | grep -wv 'mysql\|personnel\|buildings\|information_schema\|performance_schema');echo $DATABASE_LIST;mysqldump --no-data --databases $DATABASE_LIST > structure.sql
 
 
+#Dump table backup in gz file
 
+mysqldump db_name table_name | gzip > table_name.sql.gz
+
+#Restore table from gz file
+
+gunzip < table_name.sql.gz | mysql -u username -p db_name
 
 
 # SQL Commands
