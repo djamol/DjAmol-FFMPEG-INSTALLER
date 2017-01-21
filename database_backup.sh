f8d8a1b75cea39ed0c74edbf9401e2a4bbceac2e
DjAmolGroup <djamolpatil@gmail.com>
2017-01-21 18:58:10 +0530

    Update database_backup.sh

--- database_backup.sh
+++ database_backup.sh
@@ -15,6 +15,18 @@ mysqldump db_name table_name | gzip > table_name.sql.gz
 gunzip < table_name.sql.gz | mysql -u username -p db_name
 
 
+
+
+#Drop (i.e. remove tables)
+
+mysql -Nse 'show tables' DATABASE_NAME | while read table; do mysql -e "drop table $table" DATABASE_NAME; done
+
+#Truncate (i.e. empty tables)
+
+mysql -Nse 'show tables' DATABASE_NAME | while read table; do mysql -e "truncate table $table" DATABASE_NAME; done
+
+
+
 # SQL Commands
 #//delete all records/row
 # DELETE FROM `table`
