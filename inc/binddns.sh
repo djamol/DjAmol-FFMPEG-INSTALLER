f2379ebab06e6ec92078d5cc9963243b5b94cc14
DjAmolGroup <djamolpatil@gmail.com>
2017-08-01 19:58:04 +0530

    Update binddns.sh

--- inc/binddns.sh
+++ inc/binddns.sh
@@ -7,7 +7,8 @@ if yum -y install bind; then
 echo -e "\033[33;32m yum -y BIND DNS Success "
 date +"%r" >> $BUILD;echo "Success : BIND DNS Install" >> $BUILD
 chkconfig named on
-service named start
+#service named start
+chkconfig --level 35 named on
 else
 echo -e "\033[33;31m yum -y BIND DNS Failed";date +"%r" >> $BUILD;echo "Failed : BIND DNS Install" >> $BUILD
 fi
