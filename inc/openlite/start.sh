7806a44f0d4e6da94b7321cd5eeead54a2eaefd0
DjAmolGroup <djamolpatil@gmail.com>
2019-02-23 21:45:31 +0530

    Update start.sh

--- inc/openlite/start.sh
+++ inc/openlite/start.sh
@@ -1,12 +1,13 @@
-SRC_Source="/src"
-PWD_DIR="/root"
+SCRIPTPATH=$(dirname "$SCRIPT")
+##SRC_Source="/src"
+PWD_DIR=$SCRIPTPATH/conf
 cpu_num=`cat /proc/cpuinfo | grep processor | wc -l`
 
- sh jemalloc.sh 2>&1 | tee -a openl-jemalloc.txt
- sh openlite.sh 2>&1 | tee -a openl-openlite.txt
- sh nginx.sh 2>&1 | tee -a openl-jemalloc.txt
- sh php.sh 2>&1 | tee -a openl-php.txt
- sh ext-php-redis.sh 2>&1 | tee -a openl-redis.txt
- sh ext-php-mem.sh 2>&1 | tee -a openl-mem.txt
- sh ext-php-zendOPcache.sh 2>&1 | tee -a openl-zendOPcache.txt
- sh end.sh 2>&1 | tee -a openl-end.txt
+ sh $SCRIPTPATH/inc/openlite/jemalloc.sh 2>&1 | tee -a openl-jemalloc.txt
+ sh $SCRIPTPATH/inc/openlite/openlite.sh 2>&1 | tee -a openl-openlite.txt
+ sh $SCRIPTPATH/inc/openlite/nginx.sh 2>&1 | tee -a openl-jemalloc.txt
+ sh $SCRIPTPATH/inc/openlite/php.sh 2>&1 | tee -a openl-php.txt
+ sh $SCRIPTPATH/inc/openlite/ext-php-redis.sh 2>&1 | tee -a openl-redis.txt
+ sh $SCRIPTPATH/inc/openlite/ext-php-mem.sh 2>&1 | tee -a openl-mem.txt
+ sh $SCRIPTPATH/inc/openlite/ext-php-zendOPcache.sh 2>&1 | tee -a openl-zendOPcache.txt
+ sh $SCRIPTPATH/inc/openlite/end.sh 2>&1 | tee -a openl-end.txt
