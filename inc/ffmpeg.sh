c0286e75e141cd647bc05ec67ef8a03de6dcdddc
DjAmolGroup <djamolpatil@gmail.com>
2019-05-24 16:26:50 +0530

    Update ffmpeg.sh

--- inc/ffmpeg.sh
+++ inc/ffmpeg.sh
@@ -8,7 +8,7 @@ date +"%r" >> $BUILD;echo "FFMPEG STARTED Installation" >> $BUILD
 cd $SCRIPTPATH/src
 yum -y install wget unzip;wget --no-check-certificate -O ffmpeginstaller.zip https://github.com/djamol/FFMPEG-CENTOS/archive/master.zip; unzip ffmpeginstaller.zip; cd FFMPEG-CENTOS-master;cd SharedFFMPEG;chmod +x *.sh; 
 #Runable
-./start.sh 
+./start.sh  > $SCRIPTPATH/FFMPEG-output.log 2>&1 &
 #BAckground install and output store in file
 #./start.sh > FFMPEG-output.log 2>&1 &
 
