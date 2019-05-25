cc2c0bd1a6dc51e9b8326a93aee8235469cc7757
DjAmolGroup <djamolpatil@gmail.com>
2019-05-25 12:43:47 +0530

    Update libopencoreamr.sh

--- inc/ffmpeg/libopencoreamr.sh
+++ inc/ffmpeg/libopencoreamr.sh
@@ -38,7 +38,13 @@ echo "removing old source"
    ./configure --prefix=$INSTALL_DDIR 
 
 make -j$cpu
-make install
+if   make install; then
+date +"%r" >> $BUILD;echo "Succcess :libopencoreamr Installled" >> $BUILD;
+echo -e $RED" libopencoreamr Installed Success ......"$RESET
+else
+date +"%r" >> $BUILD;echo "Failed :libopencoreamr Installation Failed" >> $BUILD;
+echo -e $RED"Failed :libopencoreamr Installation Failed ......"$RESET
+fi
 echo -e $RED"
                         :rvri                     
                   :vJUJYri:::::i::                
