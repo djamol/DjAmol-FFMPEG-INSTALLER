cc2d4b0c060d15d9f3d20510be1a7671dde8134d
DjAmolGroup <djamolpatil@gmail.com>
2019-05-25 12:59:50 +0530

    Update xvid.sh

--- inc/ffmpeg/xvid.sh
+++ inc/ffmpeg/xvid.sh
@@ -36,7 +36,13 @@ echo "Removing old source"
    cd xvidcore/build/generic/
 ./configure --prefix=$INSTALL_DDIR
 make -j$cpu
-make install
+if   make install; then
+date +"%r" >> $BUILD;echo "Succcess :xvidcore Installled" >> $BUILD;
+echo -e $RED" xvidcore Installed Success ......"$RESET
+else
+date +"%r" >> $BUILD;echo "Failed :xvidcore Installation Failed" >> $BUILD;
+echo -e $RED"Failed :xvidcore Installation Failed ......"$RESET
+fi
 echo -e $RED"
                         :rvri                     
                   :vJUJYri:::::i::                
